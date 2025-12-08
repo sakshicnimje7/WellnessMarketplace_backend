7781aeb81756a27dce519a6c04ddd7c2eb31f8e1
Your Name <your.email@example.com>
2025-12-08 20:32:10 +0530

    Initial upload of project files

---
+++ src/main/java/com/infosys/wmat/repository/PractitionerProfileRepository.java
@@ -0,0 +1,7 @@
+package com.infosys.wmat.repository;
+
+import com.infosys.wmat.entity.PractitionerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PractitionerProfileRepository extends JpaRepository<PractitionerProfile, Long> {
+}
